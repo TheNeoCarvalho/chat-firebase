092d4543d182e660d813972e0eef08b90808d82d
Manoel Carvalho <manoelmecat@gmail.com>
2024-05-27 21:30:52 -0300

    feat: register user and logout

--- app/signUp.js
+++ app/signUp.js
@@ -1,8 +1,7 @@
 import { useRouter } from "expo-router";
 import React from "react";
 import { Image, Pressable, Text, TextInput, View } from "react-native";
-
-export default function SignIn() {
+export default function SignUp() {
   const router = useRouter();
   return (
     <View className="flex-1 justify-center items-center mx-8">
@@ -13,8 +12,14 @@ export default function SignIn() {
         width={300}
         height={300}
       />
-      <Text className="mb-8 text-2xl">Login</Text>
+      <Text className="mb-8 text-2xl">Cadastro</Text>
       <View className="w-full gap-2 items-center">
+        <Text className="text-left w-80">Nome</Text>
+        <TextInput
+          placeholder="Insira seu nome aqui"
+          className="w-80 h-10 bg-gray-100 p-2 rounded"
+        />
+
         <Text className="text-left w-80">Email</Text>
         <TextInput
           placeholder="Insira seu email aqui"
@@ -27,23 +32,25 @@ export default function SignIn() {
           placeholder="Insira sua senha aqui"
           className="w-80 h-10 p-2 bg-gray-100 rounded"
         />
+
+        <Text className="text-left w-80">Avatar URL</Text>
+        <TextInput
+          placeholder="Insira a url da imagem aqui"
+          className="w-80 h-10 bg-gray-100 p-2 rounded"
+        />
       </View>
-      <View className="flex-row my-4">
-        <Text className="my-4">Esqueceu a senha?</Text>
-        <Text className="my-4 mx-2 font-bold underline">Clique aqui</Text>
-      </View>
-      <Pressable className="bg-teal-300 w-80 h-10 rounded- justify-center items-center">
+
+      <Pressable className="bg-teal-300 w-80 h-10 mt-8 rounded- justify-center items-center">
         <Text className="text-xl font-bold">Entrar</Text>
       </Pressable>
-
       <View className="flex-row my-4">
-        <Text className="my-4">Não possui conta?</Text>
+        <Text className="my-4">Já possui conta?</Text>
         <Pressable
           onPress={() => {
-            router.replace("signUp");
+            router.replace("signIn");
           }}
         >
-          <Text className="my-4 mx-2 font-bold underline">Criar conta</Text>
+          <Text className="my-4 mx-2 font-bold underline">Fazer Login</Text>
         </Pressable>
       </View>
     </View>
